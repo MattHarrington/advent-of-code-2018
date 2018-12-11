220a172d9891280ff37b2224b9674a3996e55abc
Harrington <Matt.Harrington@ucsf.edu>
2018-12-11 13:32:56 -0800

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:advent_of_code_2018/advent_of_code_2018.dart' as advent_of_code_2018;
+
+main(List<String> arguments) {
+  print('Hello world: ${advent_of_code_2018.calculate()}!');
+}
