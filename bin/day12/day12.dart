7655b504a8701360d4afc94dbf3266e791f7b38b
Matt Harrington <mattharrington@users.noreply.github.com>
2018-12-12 16:26:14 -0800

    Day 12 bug identified

--- bin/day12/day12.dart
+++ bin/day12/day12.dart
@@ -4,8 +4,10 @@ import 'package:trie/trie.dart';
 import 'dart:io';
 import 'dart:collection';
 
-const DEBUG = false;
+const DEBUG = true;
+//const numberOfGenerations = 50000000000;
 const numberOfGenerations = 20;
+//const numberOfGenerations = 1000;
 
 class Pot<T> extends LinkedListEntry<Pot> {
   int number;
@@ -17,11 +19,20 @@ class Pot<T> extends LinkedListEntry<Pot> {
   String toString() => '${state}';
 }
 
+class Garden<E> extends LinkedList<Pot> {
+  @override
+  toString() {
+    var sb = StringBuffer();
+    this.forEach((pot) => sb.write(pot.state));
+    return sb.toString();
+  }
+}
+
 main() {
   var yesRules = List<String>();
   var noRules = List<String>();
 
-  var puzzleInput = File("day_12_input.txt").readAsLinesSync();
+  var puzzleInput = File("day_12_sample_input.txt").readAsLinesSync();
 
   var initialState = puzzleInput[0].split(" ")[2];
   print("initialState.length: ${initialState.length}");
@@ -35,40 +46,71 @@ main() {
   var yesTrie = Trie.list(yesRules);
   var noTrie = Trie.list(noRules);
 
-  print("All yesRules are: " + yesTrie.getAllWords().toString());
-  print("All noRules are: " + noTrie.getAllWords().toString());
-
-  // Pad the initial state with 4 empty pots to the left and 4 to the right
-  var initialPotState = LinkedList<Pot>();
-  initialPotState.add(Pot(-4, "."));
-  initialPotState.add(Pot(-3, "."));
-  initialPotState.add(Pot(-2, "."));
-  initialPotState.add(Pot(-1, "."));
-  for (var i = 0; i < initialState.length; ++i) {
-    initialPotState.add(Pot(i, initialState[i]));
+  if (DEBUG) {
+    print("All yesRules are: " + yesTrie.getAllWords().toString());
+    print("All noRules are: " + noTrie.getAllWords().toString());
   }
-  initialPotState.add(Pot(initialState.length, "."));
-  initialPotState.add(Pot(initialState.length + 1, "."));
-  initialPotState.add(Pot(initialState.length + 2, "."));
-  initialPotState.add(Pot(initialState.length + 3, "."));
-  assert(initialPotState.length == initialState.length + 8);
 
-  var generations = List<LinkedList<Pot>>();
-  generations.add(initialPotState);
+  var currentGeneration = Garden<Pot>();
 
-  for (var i = 0; i < numberOfGenerations; ++i) {
-//    if (i % 1000000000 == 0) stdout.write("${i} ..");
-    var nextGeneration = LinkedList<Pot>();
-    var iteration = 0;
-    for (var currentPot in generations[i]) {
-      int currentPotNumber = currentPot.number;
-      if (currentPot.previous == null) {
-        nextGeneration.add(Pot(currentPot.number - 4, "."));
-        nextGeneration.add(Pot(currentPot.number - 3, "."));
-        nextGeneration.add(Pot(currentPot.number - 2, "."));
-        nextGeneration.add(Pot(currentPot.number - 1, "."));
+  for (var i = 0; i < initialState.length; ++i) {
+    currentGeneration.add(Pot(i, initialState[i]));
+  }
+
+  for (var g = 0; g < numberOfGenerations; ++g) {
+    var nextGeneration = Garden<Pot>();
+//    if (DEBUG) print("${g}: ${currentGeneration.toString()}");
+    if (DEBUG) print("${g}: ${potsToString(currentGeneration)}");
+//    if (g % 100 == 0) stdout.write("${(g / 100).truncate()}C ..");
+
+    // Ensure 5 empty pots at beginning
+    int potsToAddAtBeginning = 0;
+    var tempBeginningPot = currentGeneration.first;
+    var beginningPatternStringBuffer = StringBuffer();
+    for (var i = 0; i < 5; ++i) {
+      if (tempBeginningPot.state == "#") {
+        beginningPatternStringBuffer.write("#");
+        potsToAddAtBeginning = 5 - i;
+        break;
+      } else if (tempBeginningPot.state == ".") {
+        beginningPatternStringBuffer.write(".");
       }
+      tempBeginningPot = tempBeginningPot.next;
+    }
+    var beginningPattern = beginningPatternStringBuffer.toString();
 
+    for (var i = 0; i < potsToAddAtBeginning; ++i) {
+      var firstPotNumber = currentGeneration.first.number;
+      nextGeneration.addFirst(Pot(firstPotNumber - 1, "."));
+//      if (DEBUG)
+//        print("Added pot ${currentGeneration.first.number} at beginning");
+    }
+
+    // Ensure 5 empty pots at end
+    int potsToAddAtEnd = 0;
+    var endPatternStringBuffer = StringBuffer();
+    var tempEndPot = currentGeneration.last;
+    for (var i = 0; i < 5; ++i) {
+      if (tempEndPot.state == "#") {
+        endPatternStringBuffer.write("#");
+        potsToAddAtEnd = 5 - i;
+        break;
+      } else if (tempEndPot.state == ".") {
+        endPatternStringBuffer.write(".");
+      }
+      tempEndPot = tempEndPot.previous;
+    }
+    var endPattern = endPatternStringBuffer.toString();
+
+    for (var i = 0; i < potsToAddAtEnd; ++i) {
+      var lastPotNumber = currentGeneration.last.number;
+      nextGeneration.add(Pot(lastPotNumber + 1, "."));
+//      if (DEBUG) print("Added pot ${currentGeneration.last.number} at end");
+    }
+    var lastPot = currentGeneration.last;
+
+    var iteration = 0;
+    for (var currentPot in currentGeneration) {
       var sb = StringBuffer();
       sb.write(currentPot.previous?.previous?.state ?? ".");
       sb.write(currentPot.previous?.state ?? ".");
@@ -77,67 +119,44 @@ main() {
       sb.write(currentPot.next?.next?.state ?? ".");
       var pattern = sb.toString();
 
-      var newPot = Pot(currentPot.number, "NEWPOT");
+      var newPot = Pot(currentPot.number, currentPot.state);
       if (yesTrie.getAllWords().contains(pattern)) {
         newPot.state = "#";
       } else if (noTrie.getAllWords().contains(pattern)) {
         newPot.state = ".";
       }
-      if (newPot.state == "NEWPOT") {
-        print("Pattern ${pattern} not found in either trie");
-      }
       nextGeneration.add(newPot);
-
-      if (currentPot.next == null) {
-        nextGeneration.add(Pot(currentPot.number + 1, "."));
-        nextGeneration.add(Pot(currentPot.number + 2, "."));
-        nextGeneration.add(Pot(currentPot.number + 3, "."));
-        nextGeneration.add(Pot(currentPot.number + 4, "."));
-      }
-
       ++iteration;
     }
+    currentGeneration = nextGeneration;
+//    if (DEBUG) print("currentGeneration.length: ${currentGeneration.length}");
 
-    generations.add(nextGeneration);
-
-    if (DEBUG) {
-      print("Added pots: ${nextGeneration.first.number} "
-          "${nextGeneration.first.next.number} ... "
-          "${nextGeneration.last.previous.number} "
-          "${nextGeneration.last.number}");
-    }
-  }
-
-  if (DEBUG) {
-    int generationCounter = 0;
-    for (var pots in generations) {
-      print("${generationCounter}: ${potsToString(pots)}");
-      ++generationCounter;
-    }
   }
 
   int sum = 0;
   var sb = StringBuffer();
-  for (var pot in generations[numberOfGenerations]) {
+  for (var pot in currentGeneration) {
     if (pot.state == "#") {
       sum += pot.number;
-      if (DEBUG) sb.write("${pot.number} ");
+//        if (DEBUG) sb.write("${pot.number} ");
     }
   }
-  if (DEBUG) print("Pots with plants: ${sb.toString()}");
-
   print("Sum of generation ${numberOfGenerations}: ${sum}");
+
+//    if (DEBUG) print("Pots with plants: ${sb.toString()}");
+
+  print("Number of last pot: ${currentGeneration.last.number}");
+  print("Current generation: ${potsToString(currentGeneration)}");
+//  print("currentGeneration.toString(): ${currentGeneration.toString()}");
+  if (numberOfGenerations == 20) assert(sum == 2995);
 }
 
-String potsToString(LinkedList<Pot> pots,
-    {int beginning = -100, int end = 300}) {
+String potsToString(Garden<Pot> pots, {int beginning = -2, int end = 35}) {
   var sb = StringBuffer();
   for (var pot in pots) {
     if (pot.number >= beginning && pot.number <= end) {
-      sb.write("${pot.number},");
+      sb.write("${pot.state}");
     }
   }
   return sb.toString();
 }
-// Guessed 3029 -> too high
-// Guessed 2015
