7c02fa70145589fefd1f6000e64c40983528a2cc
Matt Harrington <mattharrington@users.noreply.github.com>
2018-12-16 23:15:12 -0800

    Day 17 in progress

---
+++ bin/day17/day17.dart
@@ -0,0 +1,108 @@
+// https://adventofcode.com/2018/day/17
+
+import 'dart:io';
+
+const DEBUG = false;
+const USE_SAMPLE_DATA = true;
+
+main() {
+  var puzzleInput = (USE_SAMPLE_DATA
+      ? File("day_17_sample_input.txt").readAsLinesSync()
+      : File("day_17_input.txt").readAsLinesSync());
+  // Sample: x=495, y=2..7
+  // Sample: y=7, x=495..501
+  final rows = 14;
+  final columns = 508;
+  var grid = List.generate(rows, (_) => List<String>.filled(columns, '.'));
+
+  for (var record in puzzleInput) {
+    var recordSplit = record.split(',');
+    var xRegex = RegExp(r'(x)=(\d*\.{0,2}\d+)');
+    var xInput = xRegex.allMatches(record).elementAt(0).group(2);
+    var yRegex = RegExp(r'(y)=(\d*\.{0,2}\d+)');
+    var yInput = yRegex.allMatches(record).elementAt(0).group(2);
+    if (DEBUG) print("x: $xInput   y: $yInput");
+
+    //     var regex = RegExp(r'-?\d*\.{0,1}\d+'); // Positive or negative numbers
+    var xRangeRegex = RegExp(r'(\d*)\.{0,2}(\d*)');
+    var xRangeBegin =
+        int.parse(xRangeRegex.allMatches(xInput).elementAt(0).group(1));
+    int xRangeEnd;
+    if (xRangeRegex.allMatches(xInput).elementAt(0).group(2).trim() == "") {
+      xRangeEnd = xRangeBegin;
+    } else {
+      xRangeEnd =
+          int.parse(xRangeRegex.allMatches(xInput).elementAt(0).group(2));
+    }
+
+    var yRangeRegex = RegExp(r'(\d*)\.{0,2}(\d*)');
+    var yRangeBegin =
+        int.parse(yRangeRegex.allMatches(yInput).elementAt(0).group(1));
+    int yRangeEnd;
+    if (yRangeRegex.allMatches(yInput).elementAt(0).group(2).trim() == "") {
+      yRangeEnd = yRangeBegin;
+    } else {
+      yRangeEnd =
+          int.parse(yRangeRegex.allMatches(yInput).elementAt(0).group(2));
+    }
+
+    if (DEBUG)
+      print("x begin: $xRangeBegin x end: $xRangeEnd  "
+          "y begin: $yRangeBegin y end: $yRangeEnd");
+
+    for (var y = yRangeBegin; y <= yRangeEnd; ++y) {
+      for (var x = xRangeBegin; x <= xRangeEnd; ++x) {
+        grid[y][x] = '#';
+      }
+    }
+  }
+  grid[0][500] = '+';
+
+  print('goDown: ${goDown(500, 0, grid)}');
+  printGrid(grid);
+
+//  print('goAcross: ${goAcross(500, 6, grid)}');
+}
+
+int goDown(int x, int y, List<List<String>> grid) {
+  if (grid[y][x] == '#') {
+    // base case
+    return 0;
+  } else {
+    grid[y][x] = '~';
+    return 1 +
+        goLeft(x, y, grid) +
+        goRight(x, y, grid) +
+        goDown(x, y + 1, grid);
+  }
+}
+
+int goLeft(int x, int y, List<List<String>> grid) {
+  if (x < 400 || grid[y][x] == '#') {
+    // base case
+    return -1;
+  } else {
+    grid[y][x] = '~';
+    return 1 + goLeft(x - 1, y, grid);
+  }
+}
+
+int goRight(int x, int y, List<List<String>> grid) {
+  if (grid[y+1][x] == '.') {
+    // overflow
+    goDown(x,y,grid);
+  }
+  if (x > 507 || grid[y][x] == '#') {
+    // base case
+    return -1;
+  } else {
+    grid[y][x] = '~';
+    return 1 + goRight(x + 1, y, grid);
+  }
+}
+
+void printGrid(List<List<String>> grid) {
+  for (var row in grid) {
+    print(row);
+  }
+}
