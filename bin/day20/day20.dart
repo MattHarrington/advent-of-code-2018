e7463f5544fc07aceba3f8ced77c74ffb86efd24
Matt Harrington <mattharrington@users.noreply.github.com>
2018-12-22 08:23:58 -0800

    Day 20 part 1 complete

--- bin/day20/day20.dart
+++ bin/day20/day20.dart
@@ -5,13 +5,13 @@ import 'dart:io';
 import 'dart:math';
 
 const DEBUG = false;
-const USE_SAMPLE_DATA = true;
+const USE_SAMPLE_DATA = false;
 
 class Node {
-  String direction;
+  String value;
   List<Node> children;
 
-  Node(this.direction) {
+  Node(this.value) {
     children = List<Node>();
   }
 
@@ -19,21 +19,18 @@ class Node {
     children.add(child);
   }
 
-  static int depthOfTree(Node node) {
-    if (node.direction == r'$') {
-      return -1000;
+  int get depth {
+    if (value == r'$') {
+      return -1000; // Can be large negative number since it's passed to max()
     }
     int maxDepth = 0;
 
-    for (var child in node.children) {
-      maxDepth = max(maxDepth, depthOfTree(child));
+    for (var child in children) {
+      maxDepth = max(maxDepth, child.depth);
     }
-    stdout.write(node.direction);
+    if (DEBUG) stdout.write(value);
     return maxDepth + 1;
   }
-
-  @override
-  toString() => '';
 }
 
 main() {
@@ -41,10 +38,11 @@ main() {
   Examples:
   ^WNE$ -> 3
   ^ENWWW(NEEE|SSE(EE|N))$ -> 10
-  ^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$ -> 18 (mine is wrong)
+  ^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$ -> 18
   ^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$ -> 23
   ^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$ -> 31
    */
+  var sampleA = r'^N(E|W)N$';
   var sample3 = r'^WNE$';
   var sample10 = r'^ENWWW(NEEE|SSE(EE|N))$';
   var sample18 = r'^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$';
@@ -52,62 +50,62 @@ main() {
   var sample31 =
       r'^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$';
 
-  var puzzleInput = (USE_SAMPLE_DATA
-      ? sample10
-      : File('day_20_input.txt').readAsStringSync());
+  var puzzleInput =
+      (USE_SAMPLE_DATA ? sampleA : File('day_20_input.txt').readAsStringSync());
 
   if (DEBUG) print('Original puzzle input: $puzzleInput');
 
-  // Deal with "empty options"
+  // Deal with "empty options" like "(WNSE|)".
+  // All empty options appear to retrace their steps
+  // and end up where they started. Cutting empty options
+  // in half allows them to be accurately added to the tree.
   var emptyOptionRegex = RegExp(r'\([NEWS|]*\|\)');
-//  puzzleInput = puzzleInput.replaceAll(emptyOptionRegex, '');
-
   puzzleInput = puzzleInput.replaceAllMapped(emptyOptionRegex, (Match m) {
     int halfway = (m.group(0).length / 2).truncate();
     return '${m.group(0).substring(0, halfway)})';
   });
 
-  if (DEBUG) print('New puzzle input: $puzzleInput');
+  var tree = buildTree(puzzleInput);
 
-  var puzzleStack = Queue<String>();
-  for (var i = 1; i < puzzleInput.length; ++i) {
-    puzzleStack.add(puzzleInput[i]);
-  }
-
-  var firstNode = Node(puzzleStack.removeFirst());
-  buildTree(firstNode, puzzleStack);
-  var depth = Node.depthOfTree(firstNode);
-  print('\nDepth: $depth');
+  var depth = tree.depth;
+  if (!USE_SAMPLE_DATA) assert(depth == 4778);
+  print('\nPart one answer: $depth');
 }
 
-void buildTree(Node node, Queue<String> stack) {
-  if (stack.isEmpty) return;
-  var next = stack.removeFirst();
-//  if (next == r'$') return;
-//  if (next == ' ') return;
-  if (next == '|') return;
-  if (next == ')') return;
-  if (next == '(') {
-    // children
-    buildTree(node, stack);
-//    if (stack.isEmpty) {
-//      print("empty stack?");
-//      return;
-//    }
-//    do {
-//      next = stack.removeFirst();
-//    } while (next == ')');
-//    while (next == '|') next = stack.removeFirst();
-//  next = stack.removeFirst();
-//  if (next == '|') next = stack.removeFirst();
-//  next = stack.removeFirst();
-    stack.add(next);
-  return;
-  }
-  var newNode = Node(next);
-  node.addChild(newNode);
-  buildTree(newNode, stack);
-}
+/// Return a tree given puzzle input.
+/// Uses a stack to hold parent node when
+/// adding children.
+Node buildTree(String puzzleInput) {
+  var rootNode = Node(puzzleInput[1]);
+  var currentNode = rootNode;
 
-// 4724 too low
-// 5019 too high
+  var stack = Queue<Node>();
+
+  for (var i = 2; i < puzzleInput.length; ++i) {
+    if (puzzleInput[i] == '(') {
+      stack.addFirst(currentNode);
+    }
+
+    if (puzzleInput[i] == '|') {
+      if (puzzleInput[i + 1] == ')') {
+        currentNode = stack.removeFirst();
+        ++i;
+        continue;
+      } else {
+        currentNode = stack.first;
+      }
+    }
+
+    if (puzzleInput[i] == ')') {
+      currentNode = stack.removeFirst();
+    }
+
+    if ('NEWS'.contains(puzzleInput[i])) {
+      var childNode = Node(puzzleInput[i]);
+      currentNode.addChild(childNode);
+      currentNode = childNode;
+    }
+  }
+
+  return rootNode;
+}
