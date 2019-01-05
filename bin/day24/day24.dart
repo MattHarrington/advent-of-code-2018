278bc48777b5a479a5dd5baa14c379db11470a3a
Matt Harrington <mattharrington@users.noreply.github.com>
2019-01-04 18:28:58 -0800

    Day 24 in progress

--- bin/day24/day24.dart
+++ bin/day24/day24.dart
@@ -1,1 +1,50 @@
 // https://adventofcode.com/2018/day/24
+
+import 'dart:io';
+
+const USE_SAMPLE_DATA = true;
+
+abstract class Combatant {
+  List<Group> groups;
+}
+
+class ImmuneSystem extends Combatant {}
+
+class Infection extends Combatant {}
+
+class Group {
+  List<Unit> units;
+  int effectivePower;
+}
+
+class Unit {
+  int hitPoints;
+  int attackDamage;
+  AttackType attackType;
+  int initiative;
+  List<AttackType> weaknesses;
+  List<AttackType> immunities;
+}
+
+enum AttackType { fire, bludgeoning, slashing, cold, radiation }
+
+main() {
+  var gameOn = true;
+  while (gameOn) {
+    // Target selection
+
+    // Attack
+  }
+}
+
+List<ImmuneSystem> getImmuneSystem() {
+  var puzzleInput = USE_SAMPLE_DATA
+      ? File('day_24_sample_input.txt').readAsLinesSync()
+      : File('day_24_input.txt').readAsLinesSync();
+  for (var record in puzzleInput) {
+    if (record == 'Immune System:') continue;
+    if (record == 'Infection:') break;
+    var regex = RegExp(
+        r'(\d+) units each with (\d+) hit points (?:\(([a-z,; ]+)\))*\s*with an attack that does (\d+) ([a-z]+) damage at initiative (\d+)');
+  }
+}
